fcef04efcfffa2558081cc725e2ef8eaec8b8d28
SK <limsweekiat@gmail.com>
2025-03-27 00:02:49 +0800

    Update function to test venv

--- graph.py
+++ graph.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plot_line_graph(x: list[float], y: list[float]) -> None:
@@ -8,6 +9,7 @@ def plot_line_graph(x: list[float], y: list[float]) -> None:
     - x (list[float]): List of x values (required)
     - y (list[float]): List of y values (required)
     """
+    np.strings
     plt.plot(x, y)
     plt.savefig("test.png")
     return None
